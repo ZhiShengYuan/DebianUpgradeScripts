fb73e85fc59aace4c5f9d6d7ac1582a24fbe3e15
ZhiShengYuan <37360003+ZhiShengYuan@users.noreply.github.com>
2023-06-11 12:32:55 +0800

    fix

--- upgrade.sh
+++ upgrade.sh
@@ -4,7 +4,7 @@ cat >/etc/apt/sources.list<<EOF
 deb https://cloudflaremirrors.com/debian/ bookworm main contrib non-free non-free-firmware
 deb https://cloudflaremirrors.com/debian/ bookworm-updates main contrib non-free non-free-firmware
 deb https://cloudflaremirrors.com/debian/ bookworm-backports main contrib non-free non-free-firmware
-deb https://security.debian.org/debian-security bookworm-security main contrib non-free non-free-firmware
+deb https://security.debian.org/debian-security/ bookworm-security main contrib non-free non-free-firmware
 EOF
 apt update&&apt upgrade -y
 apt dist-upgrade -y &&apt autoremove&&reboot 
